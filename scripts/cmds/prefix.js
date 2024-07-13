fb96e6d4f86cd45465958fde53f6be29100b229a
sazidbro <166470079+sazidbro@users.noreply.github.com>
2024-07-13 15:54:55 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -9,7 +9,7 @@ module.exports = {
 		countDown: 5,
 		role: 0,
 		description: "Thay đổi dấu lệnh của bot trong box chat của bạn hoặc cả hệ thống bot (chỉ admin bot)",
-		category: "config",
+		category: "Boxchat",
 		guide: {
 			vi: "   {pn} <new prefix>: thay đổi prefix mới trong box chat của bạn"
 				+ "\n   Ví dụ:"
