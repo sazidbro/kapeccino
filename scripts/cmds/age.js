1a98a651afc8ec6f3cdf868700b339287cdeda2b
sazidbro <166470079+sazidbro@users.noreply.github.com>
2024-07-13 16:16:11 +0600

    Update age.js

--- scripts/cmds/age.js
+++ scripts/cmds/age.js
@@ -4,7 +4,7 @@ module.exports = {
     author: "Samir Œ",
     countDown: 5,
     role: 0,
-    category: "Fun",
+    category: "media",
     shortDescription: {
       en: "mention your friend and write something to post✍️",
     },
