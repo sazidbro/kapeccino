ac6e75ec2040d22200aa1ba9aab83d3abf8c5859
sazidbro <166470079+sazidbro@users.noreply.github.com>
2024-08-27 11:48:05 +0600

    Create Spotify.js

---
+++ scripts/cmds/Spotify.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+module.exports = {
+  config: { 
+    name: "spotify", 
+    countDown: 5, 
+role: 0,
+    author: "", 
+    category: "media"
+  },
+  onStart: async ({ message, args }) => {
+    if (!args[0]) return message.reply("please add a title");
+const { messageID } = await message.reply(`Downloading... "${args.join(" ")}" please wait..`);
+    try {
+      const { metadata: {title, releaseDate, artists }, link } = await spotify(`${args.join(" ")}`);
+      message.reply({
+        body: `Title: ${title}\nArtists: ${artists}\nRelease: ${releaseDate}`,
+        attachment: await global.utils.getStreamFromURL(link, "spotify.mp3"),
+      });
+message.unsend(messageID);
+    } catch (e) {
+      throw e;
+    }
+  }
+};
+
+const spotify = async (q) => {
+  try {
+    const url = `
+https://api-v1-3ciz.onrender.com`;
+    const trackUrl = (await axios.get(`${url}/spotifs?q=${encodeURIComponent(q)}`)).data[0]?.track_url;
+    if (!trackUrl) throw new Error(`${q} not found`);
+    return (await axios.get(`${url}/spotifydl?link=${trackUrl}`)).data;
+  } catch (e) {
+    return e.response?.data || e.message;
+  }
+};
